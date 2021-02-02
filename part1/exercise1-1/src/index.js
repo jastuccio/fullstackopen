6be9ea634ddd60d824d5a61d8849606888380b9c
Joe Astuccio <joe@astucc.io>
2021-02-02 14:19:48 -0500

    refactor props

--- part1/exercise1-1/src/index.js
+++ part1/exercise1-1/src/index.js
@@ -2,28 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Header from './Header'
 import Content from './Content'
-// import Total from './Total'
+import Total from './Total'
 
 const App = (props) => {
   const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const part2 = 'Using props to pass data'
+  const  part3 = 'State of a component'
+  const  exercises1 = 10
+  const  exercises2 = 7
+  const  exercises3 = 14
 
  return (
     <>
-      <Header course = {course} />
-      <Content
-        part1 = 'Fundamentals of React'
-        part2 = 'Using props to pass data'
-        part3 = 'State of a component'
-        exercises1 = {10}
-        exercises2 = {7}
-        exercises3 = {14}
+      <Header course ={course} />
+      <Content {...{part1, part2, part3, exercises1, exercises2, exercises3}}
       />
-      <Total 
-        exercises1 = {10}
-        exercises2 = {7}
-        exercises3 = {14}
-      />
-      
+      <Total {...{exercises1, exercises2, exercises3}} />
     </>
   )
 }
